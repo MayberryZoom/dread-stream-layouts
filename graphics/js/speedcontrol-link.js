df6ce8bde4f357c0b1d34d1f006646d9a12de1eb
Mayberry <mayberryzoom@gmail.com>
2024-04-06 01:52:50 -0400

    Use commentator names from run info
    The second player of each team is a commentator

--- graphics/js/speedcontrol-link.js
+++ graphics/js/speedcontrol-link.js
@@ -5,20 +5,34 @@ const categoryElement = document.getElementById('category');
 const runnerNameElements = document.getElementsByClassName('runner-name');
 const runnerPronounElements = document.getElementsByClassName('runner-pronouns');
 const runnerTimerElements = document.getElementsByClassName('runner-time');
+const commentatorElements = document.getElementsByClassName('commentator');
 
 const formatRunnerNames = () => {
-    for (const element of document.getElementsByClassName('runner-name')) {
+    for (const element of runnerNameElements) {
         let fontSize = parseInt(getComputedStyle(element).getPropertyValue('font-size'));
         const containerWidth = element.parentElement.parentElement.offsetWidth;
-    
+
         while (element.parentElement.offsetWidth + element.parentElement.offsetHeight + 10 > containerWidth) element.style.fontSize = --fontSize + 'px';
-    }    
+    }
 }
 
 const formatTimers = () => {
-    for (const element of document.getElementsByClassName('runner-time')) {
+    for (const element of runnerTimerElements) {
         element.style.right = element.parentElement.offsetWidth - element.offsetWidth - 10 + "px";
         element.style.top = element.parentElement.offsetHeight + "px";
+    }
+}
+
+const formatCommentators = () => {
+    for (const element of commentatorElements) {
+        let fontSize = parseInt(getComputedStyle(element).getPropertyValue('font-size'));
+        const parentStyles = getComputedStyle(element.parentElement);
+
+        const containerWidth = element.parentElement.clientWidth
+            - parseInt(parentStyles.getPropertyValue('padding-left'))
+            - parseInt(parentStyles.getPropertyValue('padding-right'));
+
+        while (element.offsetWidth > containerWidth) element.style.fontSize = --fontSize + 'px';
     }
 }
 
@@ -27,12 +41,12 @@ runReplicant.on('change', (newValue) => {
         categoryElement.textContent = newValue.category;
         for (let i = 0; i < runnerNameElements.length; i++) runnerNameElements[i].textContent = newValue.teams[i].players[0].name;
         for (let i = 0; i < runnerPronounElements.length; i++) runnerPronounElements[i].textContent = "(" + newValue.teams[i].players[0].pronouns + ")";
-        for (let i = 0; i < runnerTimerElements.length; i++) {
-            runnerTimerElements[i].id = newValue.teams[i].id;
-        }
+        for (let i = 0; i < runnerTimerElements.length; i++) runnerTimerElements[i].id = newValue.teams[i].id;
+        for (let i = 0; i < commentatorElements.length; i++) commentatorElements[i].textContent = newValue.teams[i].players[1].name + " (" + newValue.teams[i].players[1].pronouns + ")";
 
         formatRunnerNames();
         formatTimers();
+        formatCommentators();
     };
 });
 
